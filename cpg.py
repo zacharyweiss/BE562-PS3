5b2f2839f0968d1dfa26a4b1c39bc4f9622b35a4
zacharyweiss <zachary.weiss.acct@gmail.com>
2019-10-03 20:36:48 -0400

    Generate sequence code

--- cpg.py
+++ cpg.py
@@ -18,8 +18,27 @@ m_two = [[0.300, 0.285, 0.205, 0.210],  #A
          [0.322, 0.078, 0.298, 0.302],  #C
          [0.177, 0.292, 0.239, 0.292]]  #T
 
-def main():
+bases = ["A", "G", "C", "T"]
 
+def gen_seq(trans_prob, seq_len):
+    # initialize sequence of given length
+    s = ["Z"]*seq_len
+    # initialize first nucleotide (even probability of being any base)
+    s[0] = bases[np.where(np.random.multinomial(1, [0.25, 0.25, 0.25, 0.25]))[0][0]]
+
+    i = 1
+    while i < seq_len:
+        s[i] = bases[np.where(np.random.multinomial(1, trans_prob[bases.index(s[i-1])]))[0][0]]
+        i += 1
+
+    # join sequence into single string (attempting to optimize and avoid appending with each base)
+    seq = ''.join(s)
+    return seq
+
+
+def main():
+    seq = gen_seq(m_one, 15)
+    print(seq)
 
 
 if __name__ == "__main__":
